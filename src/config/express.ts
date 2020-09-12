cc1b6b8f598a8e0e35fa8879cb64c09c859a4b85
Anurag Chutani <a7urag@gmail.com>
2020-09-13 01:44:50 +0530

    Move express config to express.ts

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,30 @@
-import express from 'express';
 import * as bodyParser from 'body-parser';
+import express from 'express';
+const morgan = require('morgan');
+
+import authenticate from '../middlewares/authenticate';
+import application from '../constants/application';
+import indexRoute from '../routes/index.route';
+import joiErrorHandler from '../middlewares/joiErrorHandler';
+import * as errorHandler from '../middlewares/apiErrorHandler';
 
 const app = express();
 
 require('dotenv').config();
-
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.use(authenticate);
+
+// Router
+app.use(application.url.base, indexRoute);
+
+// Joi Error Handler
+app.use(joiErrorHandler);
+// Error Handler
+app.use(errorHandler.notFoundErrorHandler);
+
+app.use(errorHandler.errorHandler);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,17 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-const morgan = require('morgan');
 
-import express from './config/express';
-import application from './constants/application';
-
-import * as errorHandler from './middlewares/apiErrorHandler';
-import joiErrorHandler from './middlewares/joiErrorHandler';
 import logger from './config/logger';
-import authenticate from './middlewares/authenticate';
-
-import indexRoute from './routes/index.route';
-
+import app from './config/express';
 const PORT = process.env.PORT || 5000;
 
 createConnection()
   .then(() => {
     logger.info('database connection created');
-    express.use(morgan('dev'));
-
-    express.use(authenticate);
-
-    // Router
-    express.use(application.url.base, indexRoute);
-
-    // Joi Error Handler
-    express.use(joiErrorHandler);
-    // Error Handler
-    express.use(errorHandler.notFoundErrorHandler);
-
-    express.use(errorHandler.errorHandler);
-
-    express.listen(PORT, () => {
+    app.listen(PORT, () => {
       logger.info(`Server running at ${PORT}`);
     });
   })
   .catch((error: Error) => {
     logger.info(`Database connection failed with error ${error}`);
   });
-
-export default express;
